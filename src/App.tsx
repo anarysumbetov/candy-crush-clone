f3815af3b786d86d9c46d74038029f6d53a7b873
Anarys Umbetov <brodiaga_sirim@mail.ru>
2022-10-29 22:08:09 +0600

    Popping the Candies section has done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Board from "./components/Board";
 import { updateBoard } from "./store";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { createBoard } from "./utils/createBoard";
+import { formulaForColumnOfFour, formulaForColumnOfThree, generateInvalidMoves } from "./utils/formulas";
+import { checkForRowOfFour, checkForRowOfThree, isColumnOfFour, isColumnOfThree } from "./utils/moveCheckLogic";
 
 function App() {
 
@@ -14,6 +16,18 @@ function App() {
   useEffect(() => {
     dispatch(updateBoard(createBoard(boardSize)));
   }, [boardSize, dispatch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      checkForRowOfFour(newBoard, boardSize, generateInvalidMoves(boardSize, true));
+      isColumnOfThree(newBoard, boardSize, formulaForColumnOfThree(boardSize));
+      checkForRowOfThree(newBoard, boardSize, generateInvalidMoves(boardSize));
+      dispatch(updateBoard(newBoard))
+    }, 150);
+    return () => clearInterval(timeout)
+  }, [board, boardSize, dispatch]);
 
   return (
     <div className="flex items-center justify-center h-screen">
