24768d15c0731193ce0094cf0971e1611f8a1f62
Anarys Umbetov <brodiaga_sirim@mail.ru>
2022-10-30 16:22:03 +0600

    Dragging the Candies section has done

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { dragDrop, dragEnd, dragStart } from "../store";
+import { useAppDispatch } from "../store/hooks";
 
-const Tile = ({ candy, candyId }: { candy: string, candyId: number }) => {
+function Tile({ candy, candyId }: { candy: string, candyId: number }) {
+  const dispatch = useAppDispatch();
+
   return (
     <div 
         className="h-16 w-16 flex justify-center items-center m-0.5 rounded-lg select-none"
@@ -8,7 +11,19 @@ const Tile = ({ candy, candyId }: { candy: string, candyId: number }) => {
             boxShadow: "inset 5px 5px 15px #062525, inset -5px -5px 15px #aaaab7bb",
         }}
     >
-        {candy && <img src={candy} alt="candy" className="h-14 w-14" candy-id={candyId} />}
+        {candy && <img 
+          src={candy} 
+          alt="candy" 
+          className="h-14 w-14" 
+          candy-id={candyId} 
+          draggable={true}
+          onDragStart={(e) => dispatch(dragStart(e.target))}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={(e) => e.preventDefault()}
+          onDragLeave={(e) => e.preventDefault()}
+          onDrop={(e) => dispatch(dragDrop(e.target))}
+          onDragEnd={() => dispatch(dragEnd())}
+        />}
     </div>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,17 @@
 import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+import { dragEndReducer } from "./reducers/dragEnd";
 import { moveBelowReducer } from "./reducers/moveBelow";
 
 const initialState : {
     board: string[];
     boardSize: number;
+    squareBeingReplaced: Element | undefined;
+    squareBeingDragged: Element | undefined;
 } = {
     board: [],
     boardSize: 8,
+    squareBeingDragged: undefined,
+    squareBeingReplaced: undefined,
 };
 
 const candyCrushSlice = createSlice({
@@ -16,6 +21,13 @@ const candyCrushSlice = createSlice({
         updateBoard: (state, action: PayloadAction<string[]>) => {
             state.board = action.payload;
         },
+        dragStart: (state, action: PayloadAction<any>) => {
+            state.squareBeingDragged = action.payload;
+        },
+        dragDrop: (state, action: PayloadAction<any>) => {
+            state.squareBeingReplaced = action.payload;
+        },
+        dragEnd: dragEndReducer,
         moveBelow: moveBelowReducer,
     },
 })
@@ -24,9 +36,13 @@ export const store = configureStore({
     reducer: {
         candyCrush: candyCrushSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+        serializableCheck: false,
+        }),
 });
 
-export const { updateBoard, moveBelow } = candyCrushSlice.actions 
+export const { updateBoard, moveBelow, dragDrop, dragEnd, dragStart } = candyCrushSlice.actions 
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
